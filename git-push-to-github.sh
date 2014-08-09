0bc6116825093281a5f1b4e1350f7ffa7e0adfec
Jonathan Campbell <jonathan@castus.tv>
2014-08-09 10:30:59 -0700

    move repo, add script

---
+++ git-push-to-github.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+git remote add github https://joncampbell123@github.com/joncampbell123/msdos11-oss-style.git # might fail
+git remote set-url github https://joncampbell123@github.com/joncampbell123/msdos11-oss-style.git # might fail
+git push github master
+
